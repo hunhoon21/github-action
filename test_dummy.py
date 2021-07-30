17bdc56e44e65e995ff4077af75abc4c2b4c3610
hunhoon21 <hunhoon21@gmail.com>
2021-07-30 10:43:50 +0900

    add another test

--- test_dummy.py
+++ test_dummy.py
@@ -3,3 +3,11 @@
 def test_dummy() -> None:
     """dummy test"""
     assert True
+
+
+def test_dummy2() -> None:
+    "assert test"
+    try:
+        assert False
+    except AssertionError:
+        pass
