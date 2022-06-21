548699a9d6a2ea27c144f72162195cf8d2917ad2
hunhoon21 <hunhoon21@gmail.com>
2022-06-21 14:06:46 +0900

    :hammer: update workflow

--- scripts/upload-release.py
+++ scripts/upload-release.py
@@ -5,8 +5,33 @@ print("say hello")
 
 tag = os.environ["GITHUB_REF_NAME"]
 
-url = f"https://api.github.com/repos/hunhoon21/github-action/releases/tags/{tag}"
+is_develop = True if "dev" in tag.split("-") else False
 
+base_url = os.environ["INPUT_BASE_DEV_ADMIN"] if is_develop else os.environ["INPUT_BASE_ADMIN"]
+
+auth_url = f"{base_url}/admin_user/authenticate"
+
+email = "admin@test.com"
+pw = "12345aA!"
+body = {"email": email, "password": pw}
+
+resp = requests.post(auth_url, json=body)
+token = resp.json()["result"]
+
+auth_token = f"Bearer {token['bearer_token']}"
+headers = {"Authorization": auth_token}
+
+# Check if this release exists
+link_release_get_url = f"{base_url}/link_release/get"
+body = {
+    "link_ver": tag.split("-")[0]
+}
+resp = requests.post(link_release_get_url, headers=headers, json=body)
+
+url = f"https://api.github.com/repos/hunhoon21/github-action/releases/tags/{tag}"
 resp = requests.get(url)
-print(resp)
-print(resp.text)
+print(resp.json())
+
+link_release_id = None
+if resp.json().ok:
+    link_release_id = resp.json()["result"]["link_release"]["id"]
