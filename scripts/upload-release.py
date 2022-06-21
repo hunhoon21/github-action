1b7d9f6b4ab2f6823bd7f761c8aead26cca4ed8d
hunhoon21 <hunhoon21@gmail.com>
2022-06-21 11:00:25 +0900

    :hammer: update scripts

--- scripts/upload-release.py
+++ scripts/upload-release.py
@@ -1,1 +1,12 @@
+import os
+import requests
+
 print("say hello")
+
+tag = os.environ["GITHUB_REF_NAME"]
+
+url = f"https://api.github.com/repos/hunhoon21/github-action/releases/tags/{tag}"
+
+resp = requests.get(url)
+print(resp)
+print(resp.text)
